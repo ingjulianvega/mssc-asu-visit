45a1c8e1a263d36ea0582edd467431b5c3bc358f
Julian Vega <ingjulianvega@gmail.com>
2021-05-11 23:28:00 -0500

    :zap: WIP JMS receiving a serialized object and using properties for the configurations

--- src/main/java/ingjulianvega/ximic/events/UpdateVisitEvent.java
+++ src/main/java/ingjulianvega/ximic/events/UpdateVisitEvent.java
@@ -12,7 +12,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UpdateVisitEvent {
+public class UpdateVisitEvent implements Serializable{
+
+    static final long serialVersionUID = -1876109010441802189L;
 
     private Visit visit;
 }
